28d82c6447534e67fb7aabbf9522951a88329ca1
Divyansh Mittal <mittaldivyansh2212@gmail.com>
2022-06-02 21:27:54 +0530

    02/06/22

--- #36/forms/src/App.jsx
+++ #36/forms/src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const App = () => {
 
     const [fullName, setFullName] = useState({
-        fname: "", lname: ""
+        fname: "", lname: "", email: "", phone: ""
     })
 
     const onSubmits = (event) => {
@@ -15,26 +15,59 @@ const App = () => {
         const name = event.target.name
 
         setFullName((prev) => {
-            if (name === 'fname') {
-                return {
-                    fname: value,
-                    lname: prev.lname
-                }
-            }
-            else if (name === 'lname') {
-                return {
-                    lname: value,
-                    fname: prev.fname
-                }
+            return {
+                ...prev,
+                [name]: value
             }
         })
+
+        // setFullName((prev) => {
+        //     if (name === 'fname') {
+        //         return {
+        //             fname: value,
+        //             lname: prev.lname,
+        //             email: prev.email,
+        //             phone: prev.phone
+        //         }
+        //     }
+        //     else if (name === 'lname') {
+        //         return {
+        //             lname: value,
+        //             fname: prev.fname,
+        //             email: prev.email,
+        //             phone: prev.phone
+        //         }
+        //     }
+        //     else if (name === 'email') {
+        //         return {
+        //             email: value,
+        //             fname: prev.fname,
+        //             lname: prev.lname,
+        //             phone: prev.phone
+        //         }
+        //     }
+        //     else if (name === 'phone') {
+        //         return {
+        //             phone: value,
+        //             lname: prev.lname,
+        //             fname: prev.fname,
+        //             email: prev.email
+        //         }
+        //     }
+        // })
     }
 
     return (<>
         <form onSubmit={onSubmits}>
             <h1>Hello {fullName.fname} {fullName.lname}</h1>
+            <div>{fullName.email}</div>
+            <div>{fullName.phone}</div>
+
             <input placeholder="Enter your first name" type="text" name="fname" onChange={func} value={fullName.fname}></input>
             <input placeholder="Enter your last name" type="text" name="lname" onChange={func} value={fullName.lname}></input>
+            <input placeholder="Enter your email" type="email" name="email" onChange={func} value={fullName.email}></input>
+            <input placeholder="Enter your phone number" type="number" name="phone" onChange={func} value={fullName.phone}></input>
+
             <button type="submit">Submit</button>
         </form>
 

bf0430428aa1a0f634dfad1a833243d3ea2e6fac
Divyansh Mittal <mittaldivyansh2212@gmail.com>
2022-06-20 22:18:03 +0530

    20/06/22

---
+++ #56/router/src/HomeContent.js
@@ -0,0 +1,10 @@
+const HomeContent = () => {
+    return (
+        <>
+            <h1>Home page</h1>
+            <p>my name is Divyansh. my name is Divyansh. my name is Divyansh. my name is Divyansh. my name is Divyansh. my name is Divyansh. my name is Divyansh. my name is Divyansh. my name is Divyansh. my name is Divyansh. my name is Divyansh. my name is Divyansh. my name is Divyansh. my name is Divyansh. my name is Divyansh. my name is Divyansh. </p>
+        </>
+    )
+}
+
+export default HomeContent;
